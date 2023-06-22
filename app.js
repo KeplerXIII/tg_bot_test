66e957ed0d35c8fabb7ecc0590cb9f38f7dcc40c
a.pushkin <a.v.pushkin@gmail.com>
2023-06-22 11:55:23 +0300

    доработка

---
+++ app.js
@@ -0,0 +1,13 @@
+let tg = window.Telegram.WebApp
+let buyBtn = document.getElementById('buy')
+let orderBtn = document.getElementById('order')
+
+buyBtn.addEventListener('click', () => {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('form').style.display = 'block';
+    document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name
+})
+
+orderBtn.addEventListener('click', () => {
+    tg.close()
+})
