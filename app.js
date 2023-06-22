3f82f6a8081c9d62f577664884c73e94efb5a54a
a.pushkin <a.v.pushkin@gmail.com>
2023-06-22 16:07:17 +0300

    доработка

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
+// let tg = window.Telegram.WebApp
+// let buyBtn = document.getElementById('buy')
+// let orderBtn = document.getElementById('order')
+// tg.expand();
+
+// buyBtn.addEventListener('click', () => {
+//     document.getElementById('main').style.display = 'none'
+//     document.getElementById('form').style.display = 'block'
+//     // document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name
+// })
+
+// orderBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     document.getElementById('error').innerText = ''
+//     let name = document.getElementById('user_name').value
+//     let date = document.getElementById('date').value
+
+//     if (date) {
+//     } else {
+//         document.getElementById('error').innerText = 'Ошибка в имени'
+//         return
+//     }
+
+//     if(name.length < 5) {
+//         document.getElementById('error').innerText = 'Ошибка в имени'
+//         return
+//     }
+    
+//     let data = {
+//         name: name,
+//         date: date
+//     }
+
+//     tg.sendData(JSON.stringify(data))
+
+//     tg.close()
+//     console.log(data)
+// })
+
 let tg = window.Telegram.WebApp
-let buyBtn = document.getElementById('buy')
-let orderBtn = document.getElementById('order')
-tg.expand();
+let statsBtn = document.getElementById('stats')
+tg.expand()
 
-buyBtn.addEventListener('click', () => {
-    document.getElementById('main').style.display = 'none'
-    document.getElementById('form').style.display = 'block'
-    // document.getElementById('user_name').value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name
-})
-
-orderBtn.addEventListener('click', (e) => {
+statsBtn.addEventListener('click', (e) => {
     e.preventDefault()
     document.getElementById('error').innerText = ''
-    let name = document.getElementById('user_name').value
-    let date = document.getElementById('date').value
-
-    if (date) {
-    } else {
-        document.getElementById('error').innerText = 'Ошибка в имени'
-        return
-    }
-
-    if(name.length < 5) {
-        document.getElementById('error').innerText = 'Ошибка в имени'
-        return
-    }
+    let date1 = document.getElementById('date1').value
+    if (date1) {} else {
+                document.getElementById('error').innerText = 'Выбери дату'
+                return
+            }
     
     let data = {
-        name: name,
-        date: date
+        type: 'one_day',
+        date1: date1
     }
 
     tg.sendData(JSON.stringify(data))
-
     tg.close()
-    console.log(data)
 })
