62e8298a4ea43cbccebcf2654bb22e6d5a82bb26
a.pushkin <a.v.pushkin@gmail.com>
2023-06-22 11:21:01 +0300

    [+] Добавлены стили

---
+++ main.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+
+bot = Bot('5396061392:AAFey3qztihNxidd8btHlfye24p4XmMffV8')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть страницу', web_app=WebAppInfo(url='https://keplerxiii.github.io/tg_bot_test/')))
+    await message.answer('Привет, мой друг!', reply_markup=markup)
+
+executor.start_polling(dp)
